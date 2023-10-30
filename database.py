5b2837c811c1c584e492c67b42399fc9b7fbf090
etchedheadplate <1eenqn2j@protonmail.com>
2023-10-30 22:16:07 +0300

    database entities refactoring

--- database.py
+++ database.py
@@ -1,12 +1,17 @@
 import pandas as pd
-from data_tools import get_data
-from data_config import cryptocurrency, history_file
+from data_tools import get_data, format_things
+from data_config import cryptocurrency, database_file
 from api.coingecko import BASE
 
 list_prices_data = get_data(BASE, f'coins/{cryptocurrency}/market_chart')['prices']  # list
 list_market_caps_data = get_data(BASE, f'coins/{cryptocurrency}/market_chart')['market_caps']  # list
+list_market_total_volumes = get_data(BASE, f'coins/{cryptocurrency}/market_chart')['total_volumes']  # list
 
 prices_data = pd.DataFrame(list_prices_data, columns=['Date', 'Price'])
 market_cap_data = pd.DataFrame(list_market_caps_data, columns=['Date', 'Market Cap'])
-history_data = prices_data.merge(market_cap_data, on='Date', how='left')
-history_data.to_csv(history_file, index=False)
+total_volumes_data = pd.DataFrame(list_market_total_volumes, columns=['Date', 'Total Volumes'])
+history_data = prices_data.merge(market_cap_data, on='Date', how='left').merge(total_volumes_data, on='Date', how='left')
+history_data.to_csv(database_file, index=False)
+df = pd.read_csv(database_file)
+df['Supply Circulating'] = df['Market Cap'] / df['Price']
+df.to_csv(database_file, index=False)
--- market_chart.py
+++ market_chart.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from data_tools import format_currency, convert_timestamp_to_utc, select_chart_background
-from data_config import ticker, database_file, chart_path, chart_font
+from data_config import ticker, database_file, chart_path, chart_font, chart_background_image
 from api.coingecko import BASE
 from matplotlib.ticker import FuncFormatter
 from matplotlib import font_manager
@@ -21,14 +21,14 @@ ax1.set_ylabel('Price', color='orange')
 ax1.yaxis.set_major_formatter(FuncFormatter(lambda x, _: format_currency(x, ticker)))
 ax1.tick_params(axis='y', labelcolor='orange')
 
-# Plot Market Cap on the right axis in green
-line2, = ax2.plot(df['Date'], df['Market Cap'], color='green', label='Market Cap')
-ax2.set_ylabel('Market Cap', color='green')
+# Plot Total Volumes on the right axis in green
+line2, = ax2.plot(df['Date'], df['Total Volumes'], color='green', label='Total Volumes')
+ax2.set_ylabel('Total Volumes', color='green')
 ax2.yaxis.set_major_formatter(FuncFormatter(lambda x, _: format_currency(x, ticker)))
 ax2.tick_params(axis='y', labelcolor='green')
 
 # Combine legends for both axes with South Park font
-ax1.legend([line1, line2], ['Price', 'Market Cap'], loc='upper left', prop={'family': 'South Park', 'size': 12})
+ax1.legend([line1, line2], ['Price', 'Total Volumes'], loc='upper left', prop={'family': 'South Park', 'size': 12})
 
 # Set the font properties for the text elements on the plot
 prop = font_manager.FontProperties(fname=chart_font) # Use the specified font path
@@ -36,7 +36,7 @@ prop = font_manager.FontProperties(fname=chart_font) # Use the specified font pa
 ax1.set_title('Graph Title', fontproperties=prop)
 ax1.set_xlabel('Date', fontproperties=prop)
 ax1.set_ylabel('Price', color='orange', fontproperties=prop)
-ax2.set_ylabel('Market Cap', color='green', fontproperties=prop)
+ax2.set_ylabel('Total Volumes', color='green', fontproperties=prop)
 
 # Set font properties for tick labels on both axes
 for label in ax1.get_xticklabels() + ax1.get_yticklabels() + ax2.get_yticklabels():
@@ -47,7 +47,7 @@ fig.patch.set_facecolor('none') # Set transparent background outside the plot ar
 dpi_value = 150 # Set DPI for plot image
 plt.savefig(chart_path, bbox_inches='tight', transparent=True, dpi=dpi_value) # Save the plot to a PNG file with a transparent background
 
-background = Image.open(history_chart_background) # Open background image
+background = Image.open('src/images/backgrounds/superbad.png') # Open background image
 overlay = Image.open(chart_path) # Open plot image
 x = 750  # X-coordinate for plot image
 y = 85  # Y-coordinate for plot image
